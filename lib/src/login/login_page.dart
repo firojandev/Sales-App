5edb58913cfb3fc1e0df51dfbcba8f4345bab5e7
Md Altaf Hoshain <altaf@squaregroup.com>
2024-07-04 16:43:03 +0600

    Animated is added

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -18,11 +18,18 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _empIdController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool? _rememberMe = false;
+  bool _isCardVisible = false;
 
   @override
   void initState()  {
     super.initState();
     _initializeRememberMe();
+    // Start the animation after the initial build
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _isCardVisible = true;
+      });
+    });
   }
 
   Future<void> _initializeRememberMe() async {
@@ -47,138 +54,147 @@ class _LoginPageState extends State<LoginPage> {
           color: MyColors.backgroundLight, // Your background color
         ),
         child: Center(
-          child: Card(
-            color: MyColors.whiteColor,
-            elevation: 8.0,
-            margin: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Text(AppConstants.appName,style:MyTextStyle.titleStyle),
-                  const SizedBox(height: 10.0),
-                  Text(AppConstants.loginToYourAccount,style: MyTextStyle.textStyle,),
-                  const SizedBox(height: 30.0),
-                  TextField(
-                    controller: _empIdController,
-                    decoration: InputDecoration(
-                      labelText: 'Employee ID',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 6.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOut,
+            alignment: _isCardVisible ? Alignment.center : Alignment.bottomCenter,
+            child: AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              opacity: _isCardVisible ? 1.0 : 0.0,
+              child: Card(
+                color: MyColors.whiteColor,
+                elevation: 8.0,
+                margin: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Checkbox(
-                        value: _rememberMe,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            _rememberMe = value!;
-                          });
-                        },
+                      const Text(AppConstants.appName,style:MyTextStyle.titleStyle),
+                      const SizedBox(height: 10.0),
+                      Text(AppConstants.loginToYourAccount,style: MyTextStyle.textStyle,),
+                      const SizedBox(height: 30.0),
+                      TextField(
+                        controller: _empIdController,
+                        decoration: InputDecoration(
+                          labelText: 'Employee ID',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
                       ),
-                      const Text('Remember Me'),
+                      const SizedBox(height: 16.0),
+                      TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 6.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Checkbox(
+                            value: _rememberMe,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _rememberMe = value!;
+                              });
+                            },
+                          ),
+                          const Text('Remember Me'),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () async {
+                          bool success = await Provider.of<LoginViewModel>(context,
+                                  listen: false)
+                              .login(
+                            _empIdController.text,
+                            _passwordController.text,
+                              _rememberMe!,
+                          );
+                          if (success) {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const DashboardPage()),
+                              (route) => false,
+                            );
+                          } else {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text('Login Failed'),
+                                content:
+                                    const Text('Invalid Employee ID or Password'),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.green),
+                          minimumSize: MaterialStateProperty.all<Size>(
+                              const Size(double.infinity, 48)),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: MyColors.whiteColor),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                        child: const Text(
+                          'Don\'t have an account? Register here',
+                          style: MyTextStyle.textLinkStyle,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+
                     ],
                   ),
-                  const SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: () async {
-                      bool success = await Provider.of<LoginViewModel>(context,
-                              listen: false)
-                          .login(
-                        _empIdController.text,
-                        _passwordController.text,
-                          _rememberMe!,
-                      );
-                      if (success) {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const DashboardPage()),
-                          (route) => false,
-                        );
-                      } else {
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: const Text('Login Failed'),
-                            content:
-                                const Text('Invalid Employee ID or Password'),
-                            actions: [
-                              TextButton(
-                                child: const Text('OK'),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green),
-                      minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(double.infinity, 48)),
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: MyColors.whiteColor),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterPage()),
-                      );
-                    },
-                    child: const Text(
-                      'Don\'t have an account? Register here',
-                      style: MyTextStyle.textLinkStyle,
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-
-                ],
+                ),
               ),
             ),
           ),
--- lib/src/register/register_page.dart
+++ lib/src/register/register_page.dart
@@ -19,6 +19,19 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _mobileController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  bool _isCardVisible = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _isCardVisible = true;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,131 +43,140 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Card(
-            color: MyColors.whiteColor,
-            elevation: 8.0,
-            margin: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  const SizedBox(height: 10.0),
-                  TextField(
-                    controller: _empIdController,
-                    decoration: InputDecoration(
-                      labelText: 'Employee ID',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: _mobileController,
-                    decoration: InputDecoration(
-                      labelText: 'Mobile',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: const BorderSide(
-                          color: Colors.green,
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                    obscureText: true,
-                  ),
-
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () async {
-                      String regId = await Provider.of<RegistrationViewModel>(context, listen: false).register(
-                        _empIdController.text,
-                        _mobileController.text,
-                        _passwordController.text,
-                        "device_info",
-                        "device_token",
-                      );
-                      print(regId);
-                      if (regId.isNotEmpty && regId != "0") {
-                        print("Registration successful");
-                        Navigator.pop(context);
-                      } else {
-                        // Show error message
-                        print("Registration failed");
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            title: const Text('Registration Failed'),
-                            content: const Text('Failed to register. Please try again.'),
-                            actions: [
-                              TextButton(
-                                child: const Text('OK'),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                            ],
+          child: AnimatedContainer(
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOut,
+            alignment: _isCardVisible ? Alignment.center : Alignment.bottomCenter,
+            child: AnimatedOpacity(
+              duration: const Duration(seconds: 1),
+              opacity: _isCardVisible ? 1.0 : 0.0,
+              child: Card(
+                color: MyColors.whiteColor,
+                elevation: 8.0,
+                margin: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 10.0),
+                      TextField(
+                        controller: _empIdController,
+                        decoration: InputDecoration(
+                          labelText: 'Employee ID',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
                           ),
-                        );
-                      }
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
-                      minimumSize: MaterialStateProperty.all<Size>(
-                          const Size(double.infinity, 48)),
-                    ),
-                    child: const Text('Register Now',style: MyTextStyle.textButtonStyle),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      TextField(
+                        controller: _mobileController,
+                        decoration: InputDecoration(
+                          labelText: 'Mobile',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () async {
+                          String regId = await Provider.of<RegistrationViewModel>(context, listen: false).register(
+                            _empIdController.text,
+                            _mobileController.text,
+                            _passwordController.text,
+                            "device_info",
+                            "device_token",
+                          );
+                          print(regId);
+                          if (regId.isNotEmpty && regId != "0") {
+                            print("Registration successful");
+                            Navigator.pop(context);
+                          } else {
+                            // Show error message
+                            print("Registration failed");
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text('Registration Failed'),
+                                content: const Text('Failed to register. Please try again.'),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () => Navigator.pop(context),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                          minimumSize: MaterialStateProperty.all<Size>(
+                              const Size(double.infinity, 48)),
+                        ),
+                        child: const Text('Register Now',style: MyTextStyle.textButtonStyle),
+                      ),
+                      const SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          'Already have an account? Login here',
+                          style: MyTextStyle.textLinkStyle,
+                        ),
+                      ),
+                      const SizedBox(height: 10.0),
+                    ],
                   ),
-                  const SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      'Already have an account? Login here',
-                      style: MyTextStyle.textLinkStyle,
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                ],
+                ),
               ),
             ),
           ),
